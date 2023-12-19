0346d77da1023edc3bb91cc6843ceef9a3be5460
FrancescaPicoco <francesca.picoco33@gmail.com>
2023-12-19 15:57:55 +0100

    stampo i caratteri

--- password.php
+++ password.php
@@ -1,5 +1,6 @@
 <?php 
-$caratteri = ["abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!$&?"]
+$caratteri = "a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 1 2 3 4 5 6 7 8 9 0 ! $ & ?";
+$caratteresingolo = explode(" " , "$caratteri");
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,6 +11,11 @@ $caratteri = ["abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!$&
     <title>Document</title>
 </head>
 <body>
+    <h1><?php /*print_r($caratteresingolo)*/
+    foreach ($caratteresingolo as $caratteresingolo){
+        echo "<p>$caratteresingolo</p>";
+    }
+    ?></h1>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
