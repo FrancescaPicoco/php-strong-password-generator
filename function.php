4511d13542276ac618a197273f643ac3fc241896
FrancescaPicoco <francesca.picoco33@gmail.com>
2023-12-20 09:42:33 +0100

    fatar error too byte

--- function.php
+++ function.php
@@ -3,7 +3,7 @@
 //$number = $_GET['numeri'] ?? "null";
 //$special = $_GET['speciali'] ?? "null";
 ?>
-    <?php  
+<?php  
     function rndPsw($lenght){
         $password = [];
         if (isset($_GET['letter'])) {
@@ -12,7 +12,7 @@
             for ($i=0; $i<$lenght; $i++){
                $indexL = array_rand($lettereINarray);
                $password[] = $indexL;
-           }
+            }
         }else{
             $caratteri = "a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 1 2 3 4 5 6 7 8 9 0 ! $ & ?";
             $caratteriINarray = explode(" " , "$caratteri");
